ecba32e7aeb62a0522aeea4e4bfa4224a4aa226d
Johan Kohlin <johan.kohlin@consid.se>
2023-03-05 17:14:36 +0100

    Now completely offline

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Bas
 TODO: Uninstall Purchase packages
 TODO: unlink Purchase packages
+https://api.bas.se/index.html
--- src/Api.js
+++ src/Api.js
@@ -1,89 +1,105 @@
 import bas2022 from './bas2022.json'
-import {storage} from './Storage'
+//import { storage } from './Storage'
 export const getBas = async setBas => {
-  const basCache = storage.getString('bas')
-  // TODO: kontrollera att basCache sparats i storage
-  if (typeof basCache !== 'undefined') {
-    //console.log('SET_CACHE', JSON.parse(basCache).data)
-    setBas({
-      type: 'SET_BAS_CACHE',
-      payload: {
-        loaded: true,
-        data: JSON.parse(basCache).data,
-        status: 'cache',
-        version: 'BAS 2022 ver 1.0',
-      },
-    })
-    return true
-  }
-  const response = await getData() // {loaded: true, data: json, status: 'success', version: json.ChartVersion.Name}
+  setBas({
+    type: 'SET_BAS_FILE',
+    payload: {
+      loaded: true,
+      data: bas2022,
+      status: 'file',
+      version: 'BAS 2022 ver 1.0',
+    },
+  })
+  return true
 
-  if (response.loaded) {
-    const isCurrent = response.version.includes('2022')
-    storage.set('bas', JSON.stringify(response))
-    if (isCurrent) {
-      setBas({
-        type: 'SET_BAS_LIVE',
-        payload: {
-          loaded: true,
-          data: response.data,
-          status: 'live',
-          version: response.version,
-        },
-      })
-    } else {
-      setBas({
-        type: 'SET_BAS_FILE',
-        payload: {
-          loaded: true,
-          data: bas2022,
-          status: 'file',
-          version: 'BAS 2022 ver 1.0',
-        },
-      })
-    }
-    return true
-  } else {
-    setBas({
-      type: 'SET_BAS_ERROR',
-      payload: {
-        loaded: true,
-        data: bas2022,
-        status: response.status,
-        version: 'BAS 2022 ver 1.0',
-      },
-    })
-    return true
-  }
+  // const basCache = storage.getString('bas')
+  // // TODO: kontrollera att basCache sparats i storage
+  // if (typeof basCache !== 'undefined') {
+  //   //console.log('SET_CACHE', JSON.parse(basCache).data)
+  //   setBas({
+  //     type: 'SET_BAS_CACHE',
+  //     payload: {
+  //       loaded: true,
+  //       data: JSON.parse(basCache).data,
+  //       status: 'cache',
+  //       version: 'BAS 2022 ver 1.0',
+  //     },
+  //   })
+  //   return true
+  // }
+  // const response = await getData() // {loaded: true, data: json, status: 'success', version: json.ChartVersion.Name}
+
+  // if (response.loaded) {
+  //   const isCurrent = response.version.includes('2022')
+  //   storage.set('bas', JSON.stringify(response))
+  //   if (isCurrent) {
+  //     setBas({
+  //       type: 'SET_BAS_LIVE',
+  //       payload: {
+  //         loaded: true,
+  //         data: response.data,
+  //         status: 'live',
+  //         version: response.version,
+  //       },
+  //     })
+  //   } else {
+  //     setBas({
+  //       type: 'SET_BAS_FILE',
+  //       payload: {
+  //         loaded: true,
+  //         data: bas2022,
+  //         status: 'file',
+  //         version: 'BAS 2022 ver 1.0',
+  //       },
+  //     })
+  //   }
+  //   return true
+  // } else {
+  //   setBas({
+  //     type: 'SET_BAS_ERROR',
+  //     payload: {
+  //       loaded: true,
+  //       data: bas2022,
+  //       status: response.status,
+  //       version: 'BAS 2022 ver 1.0',
+  //     },
+  //   })
+  //   return true
+  // }
 }
-export const getData = async (noData = false) => {
-  const url = 'https://api.bas.se/v1/accounts/?organizationType=ALL'
-  try {
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-      },
-    })
-    if (response.ok) {
-      const json = await response.json()
-      return noData
-        ? {
-            loaded: true,
-            data: {},
-            status: 'success',
-            version: json.ChartVersion.Name,
-          }
-        : {
-            loaded: true,
-            data: json,
-            status: 'success',
-            version: json.ChartVersion.Name,
-          }
-    } else {
-      return {loaded: false, data: {}, status: 'syntax error', version: ''}
-    }
-  } catch (error) {
-    return {loaded: false, data: error, status: 'connection error', version: ''}
-  }
-}
+// export const getData = async (noData = false) => {
+//   const url = 'https://api.bas.se/v1/accounts/?organizationType=ALL'
+//   try {
+//     const response = await fetch(url, {
+//       method: 'GET',
+//       headers: {
+//         Accept: 'application/json',
+//       },
+//     })
+//     if (response.ok) {
+//       const json = await response.json()
+//       return noData
+//         ? {
+//           loaded: true,
+//           data: {},
+//           status: 'success',
+//           version: json.ChartVersion.Name,
+//         }
+//         : {
+//           loaded: true,
+//           data: json,
+//           status: 'success',
+//           version: json.ChartVersion.Name,
+//         }
+//     } else {
+//       return { loaded: false, data: {}, status: 'syntax error', version: '' }
+//     }
+//   } catch (error) {
+//     return {
+//       loaded: false,
+//       data: error,
+//       status: 'connection error',
+//       version: 'Det gick inte att hämta data från BAS',
+//     }
+//   }
+// }
